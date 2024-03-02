68255e99a990c0af57ed9a2bed141662bd06cbc5
GalvisDev <galvisdeveloper@gmail.com>
2024-03-02 10:58:02 -0500

    checkLocationPermission

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -3,10 +3,11 @@ import { Platform, PlatformOSType } from "react-native";
 import { PERMISSIONS, PermissionStatus as RNPermissionStatus, openSettings, request } from 'react-native-permissions';
 import { PermissionStatus } from "../../infraestructure/interfaces/permissions"
 
+let status: RNPermissionStatus = 'unavailable';
+
 export const requestLocationPermission = async (): Promise<PermissionStatus> => {
 
     try {
-        let status: RNPermissionStatus = 'unavailable';
 
         let STATUS_PLATFORM: Record<PlatformOSType, PermissionStatus> = {
             ios: await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE),
@@ -23,6 +24,38 @@ export const requestLocationPermission = async (): Promise<PermissionStatus> => 
             await openSettings();
             //TODO: return await // checkLocationPermission();
         }
+
+        const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+            granted: 'granted',
+            denied: 'denied',
+            blocked: 'blocked',
+            unavailable: 'unavailable',
+            limited: 'limited',
+        }
+
+        return permissionMapper[status] ?? 'unavailable';
+
+    } catch (error) {
+        console.error('Error al solicitar permiso de ubicación:', error);
+        return 'unavailable';
+    }
+
+}
+
+export const checkLocationPermission = async (): Promise<PermissionStatus> => {
+
+    try {
+
+        let STATUS_PLATFORM: Record<PlatformOSType, PermissionStatus> = {
+            ios: await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE),
+            android: await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION),
+            windows: 'unavailable',
+            macos: 'unavailable',
+            web: 'unavailable',
+            native: 'unavailable',
+        };
+
+        status = STATUS_PLATFORM[Platform.OS];
 
         const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
             granted: 'granted',
